af89889eb049fdf2481a5e24dc93742cc4299da4
Lucas Daniel <ludaniel.sd@gmail.com>
2023-01-07 23:53:29 -0300

    adicionando o exercico 2 , calculando a media dee varias idades até o usuario entrar com um numero negativo

---
+++ Estrutura_Repetitiva/Problema_media_idades.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Problema_media_idades {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		int  idade,contador = 0,nova_idade = 0;
+		float media = 0;
+		
+		System.out.println("Digite as idades ");
+		idade = ler.nextInt();
+		
+		if(idade < 0) {
+			System.out.println("Impossivel calcular ");
+		}
+		else {
+			while(nova_idade >= 0) {
+				idade = idade + nova_idade;
+				contador++;
+				nova_idade = ler.nextInt();
+			}	
+			media = (float)(idade/contador);
+			System.out.println("Media = "+ media);
+		}	
+	}
+}
