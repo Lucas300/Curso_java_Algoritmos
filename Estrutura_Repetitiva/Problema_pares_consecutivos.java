f17d5d25ded03597cf84723393a0a870db47f276
Lucas Daniel <ludaniel.sd@gmail.com>
2023-01-08 01:37:13 -0300

    atualizando o exercicio 7  , um pouco menor e mais logico

--- Estrutura_Repetitiva/Problema_pares_consecutivos.java
+++ Estrutura_Repetitiva/Problema_pares_consecutivos.java
@@ -1,33 +1,23 @@
 import java.util.Scanner;
 
-public class Problema_pares_consecutivos {
+public class Problema_pares_concecutivos {
 
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
 		
-		int contador = 0, x = 0,soma=0;
-		System.out.print("Digite um numero inteiro: ");
+		int contador = 0,x = 0,soma=0;
+		System.out.println("Digite um numero inteiro: ");
 		x = ler.nextInt();
 		
-		if(x>0) {
+		if(x != 0) {
 			while(contador <5) {	
 				soma = soma + x +(contador*2);
 				contador++;
-			
+				if((x%2==1)&&(x>0)) {
+					soma = soma + 1;
+				}
 			}
-		}
-		else if((x%2!=0)&&(x>0)){
-			while(contador <5) {	
-				soma = soma + x +(contador*2);
-				contador++;
-				soma = soma + 1;
-			}
-			
-			
-		}
-		else if(x%2 == 0){
-			contador = 5;
-		}
-		System.out.println("SOMA = "+soma);
+			System.out.println("SOMA = "+soma);
+		}	
 	}
 }
