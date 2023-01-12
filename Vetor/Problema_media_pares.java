fc4519869d70489cf2577402fac486aa138eaa53
Lucas Daniel <ludaniel.sd@gmail.com>
2023-01-12 09:52:15 -0300

    Adicionando o exercicio 8, mostrando a media dos numeros pares digitados em um vetor

--- Vetor/Problema_media_pares.java
+++ Vetor/Problema_media_pares.java
@@ -1,7 +1,7 @@
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class Problema_abaixo_da_media {
+public class Problema_media_pares {
 
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
@@ -9,23 +9,28 @@ public class Problema_abaixo_da_media {
 		
 		System.out.println("Quantos numeros voce vai digitar");
 		int n = ler.nextInt();
-		float soma = 0,media =0;
+		
+		float soma = 0,media =0,pares = 0;
+		
 		float a[] = new float[n];
 		
 		
 		//i de indice
 		for(int i = 0;i < n; i++) {
-			System.out.print("Digite um numero: ");
+			System.out.print("Digite um numero:");
 			a[i] = ler.nextFloat();
-			soma = soma + a[i]; 
-		}
-		media = soma/n;
-		System.out.println("Media do vetor = "+media);
-		System.out.println("ELEMENTOS ABAiXO DA MEDIA: ");
-		for(int i = 0;i < n; i++){
-			if(a[i]<media) {
-				System.out.println(a[i]);
+			if(a[i]%2==0) {
+				soma = soma+a[i];
+				pares = pares + 1;
 			}
+		}
+		
+		media = soma/pares;
+		if(pares>0){
+			System.out.println(media);
+		}
+		else {
+			System.out.println("NENHUM NUMERO PAR");
 		}
 	}
 }
