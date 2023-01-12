6282ca5ddcb868663738f66b1197f6b78b276a37
Lucas Daniel <ludaniel.sd@gmail.com>
2023-01-11 23:50:07 -0300

    Adicionando o primeiro exercicio de vetor , onde o usuario digitara no maximo 10 numeros e sera mostrado no final os numeros negativos

---
+++ Vetor/Problema_negativos.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Problema_negativos {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		int n[] = new int[10];
+		
+		
+		System.out.print("Quantos numeros você vai digitar?");
+		int i = ler.nextInt(); 
+		
+		for (int contador = 0; contador < i; contador++) {
+			System.out.print("Digite um numero: ");
+			n[contador] = ler.nextInt();
+		}
+		System.out.println("Numeros Negativos:");
+		
+		for (int contador = 0; contador < i; contador++) {
+			if(n[contador]<0) {
+				System.out.println(n[contador]);
+			}
+		}
+
+	}
+
+}
